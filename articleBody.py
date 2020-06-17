fcb2ec50ae70a67cf082c368c0976365320945b5
Gwanghyeon Gim <harryghgim@gmail.com>
2020-06-17 20:11:33 +0900

    사진있으면 빼는 것 삭제

--- articleBody.py
+++ articleBody.py
@@ -8,8 +8,6 @@ def getArticleBody(url):
     soup = bs4.BeautifulSoup(htmlFile, 'html.parser')
     targetText = soup.select('#articleBodyContents')[0].stripped_strings
     for string in targetText:
-        if '사진' in string or '그래픽' in string:
-            continue
         print(f'  {string}')
 
 if __name__ == '__main__':
