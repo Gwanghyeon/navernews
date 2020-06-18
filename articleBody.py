0f47fa0e81ff42ea7a8cc939a266fff0d44f9c8a
Gwanghyeon Gim <harryghgim@gmail.com>
2020-06-18 17:03:10 +0900

    code neater

--- articleBody.py
+++ articleBody.py
@@ -1,13 +1,16 @@
 import bs4, requests
 
 # this file is imported to navernews.py
+url = 'https://news.naver.com/main/ranking/read.nhn?mid=etc&sid1=111&rankingType=popular_day&oid=079&aid=0003373061&date=20200617&type=1&rankingSeq=6&rankingSectionId=100'
 
 def getArticleBody(url):
     r = requests.get(url)
     htmlFile = r.text
-    soup = bs4.BeautifulSoup(htmlFile, 'html.parser')
+    soup = bs4.BeautifulSoup(htmlFile, 'lxml')
     targetText = soup.select('#articleBodyContents')[0].stripped_strings
     for string in targetText:
+        if '사진' in string or '그래픽' in string:
+            continue
         print(f'  {string}')
 
 if __name__ == '__main__':
--- navernews.py
+++ navernews.py
@@ -1,164 +1,40 @@
-import requests, sys
+import requests, sys, re
 from bs4 import BeautifulSoup
-from pprint import pprint
-import re, sys
 from datetime import datetime
 from articleBody import getArticleBody
 
-result = requests.get('https://news.naver.com/main/home.nhn')
-src = result.content
-soup = BeautifulSoup(src, 'lxml')
+r = requests.get('https://news.naver.com/main/home.nhn')
+htmlFile = r.text
+soup = BeautifulSoup(htmlFile, 'lxml')
 d = datetime.now()
+base_url = 'https://news.naver.com/'
+section = ['pol', 'eco', 'soc', 'lif', 'wor', 'sci']
+sectionName = ['정치', '경제', '사회', '생활/문화', '세계', 'IT/과학']
+timeFormat = d.strftime("%m월%d일 %I:%M %p")
 
-class News:
-
-    base_url = 'https://news.naver.com/'
-    section = ['pol', 'eco', 'soc', 'lif', 'wor', 'sci']
-    sectionName = ['정치', '경제', '사회', '생활/문화', '세계', 'IT/과학']
-    timeFormat = d.strftime("%m월%d일 %I:%M %p")
-
-    def politics(self):
-        base_url = self.base_url
-        section = self.section
-        sectionName = self.sectionName
-        timeFormat = self.timeFormat
-        tag_all = soup.find_all(class_=re.compile("rig.rank"+section[0]))
-        newsUrls = []
-        print("\n가장 많이 본 뉴스:"+sectionName[0], timeFormat)
-        for i, headline in enumerate(tag_all, start=1):
-            url = self.base_url + headline.attrs['href']
-            title = headline.text
-            print(f'{i} | {title}\n{url}')
-            newsUrls.append(url)
-        try:
-            choice = int(input('\n읽고 싶으신 기사 번호를 선택하세요: ')) - 1
-        except ValueError:
-            sys.exit()
-        print()
-        print(f'{choice + 1}| {tag_all[choice].text}')
-        url = newsUrls[choice]
-        getArticleBody(url)
-
-    def economy(self):
-        base_url = self.base_url
-        section = self.section
-        sectionName = self.sectionName
-        timeFormat = self.timeFormat
-        tag_all = soup.find_all(class_=re.compile("rig.rank"+section[1]))
-        newsUrls = []
-        print("\n가장 많이 본 뉴스:"+sectionName[1], timeFormat)
-        for i, headline in enumerate(tag_all, start=1):
-            url = self.base_url + headline.attrs['href']
-            title = headline.text
-            print(f'{i} | {title}\n{url}')
-            newsUrls.append(url)
-        try:
-            choice = int(input('\n읽고 싶으신 기사 번호를 선택하세요: ')) - 1
-        except ValueError:
-            sys.exit()
-        print()
-        print(f'{choice + 1}| {tag_all[choice].text}')
-        url = newsUrls[choice]
-        getArticleBody(url)
-
-    def society(self):
-        base_url = self.base_url
-        section = self.section
-        sectionName = self.sectionName
-        timeFormat = self.timeFormat
-        tag_all = soup.find_all(class_=re.compile("rig.rank"+section[2]))
-        newsUrls = []
-        print("\n가장 많이 본 뉴스:"+sectionName[2], timeFormat)
-        for i, headline in enumerate(tag_all, start=1):
-            url = self.base_url + headline.attrs['href']
-            title = headline.text
-            print(f'{i} | {title}\n{url}')
-            newsUrls.append(url)
-        try:
-            choice = int(input('\n읽고 싶으신 기사 번호를 선택하세요: ')) - 1
-        except ValueError:
-            sys.exit()
-
-        print()
-        print(f'{choice + 1}| {tag_all[choice].text}')
-        url = newsUrls[choice]
-        getArticleBody(url)
-
-    def life(self):
-        base_url = self.base_url
-        section = self.section
-        sectionName = self.sectionName
-        timeFormat = self.timeFormat
-        tag_all = soup.find_all(class_=re.compile("rig.rank"+section[3]))
-        newsUrls = []
-        print("\n가장 많이 본 뉴스:"+sectionName[3], timeFormat)
-        for i, headline in enumerate(tag_all, start=1):
-            url = self.base_url + headline.attrs['href']
-            title = headline.text
-            print(f'{i} | {title}\n{url}')
-            newsUrls.append(url)
-        try:
-            choice = int(input('\n읽고 싶으신 기사 번호를 선택하세요: ')) - 1
-        except ValueError:
-            sys.exit()
-
-        print()
-        print(f'{choice + 1}| {tag_all[choice].text}')
-        url = newsUrls[choice]
-        getArticleBody(url)
-
-    def world(self):
-        base_url = self.base_url
-        section = self.section
-        sectionName = self.sectionName
-        timeFormat = self.timeFormat
-        tag_all = soup.find_all(class_=re.compile("rig.rank"+section[4]))
-        newsUrls = []
-        print("\n가장 많이 본 뉴스:"+sectionName[4], timeFormat)
-        for i, headline in enumerate(tag_all, start=1):
-            url = self.base_url + headline.attrs['href']
-            title = headline.text
-            print(f'{i} | {title}\n{url}')
-            newsUrls.append(url)
-        try:
-            choice = int(input('\n읽고 싶으신 기사 번호를 선택하세요: ')) - 1
-        except ValueError:
-            sys.exit()
-        print()
-        print(f'{choice + 1}| {tag_all[choice].text}')
-        url = newsUrls[choice]
-        getArticleBody(url)
-
-    def science(self):
-        base_url = self.base_url
-        section = self.section
-        sectionName = self.sectionName
-        timeFormat = self.timeFormat
-        tag_all = soup.find_all(class_=re.compile("rig.rank"+section[5]))
-        newsUrls = []
-        print("\n가장 많이 본 뉴스:"+sectionName[5], timeFormat)
-        for i, headline in enumerate(tag_all, start=1):
-            url = self.base_url + headline.attrs['href']
-            title = headline.text
-            print(f'{i} | {title}\n{url}')
-            newsUrls.append(url)
-        try:
-            choice = int(input('\n읽고 싶으신 기사 번호를 선택하세요: ')) - 1
-        except ValueError:
-            sys.exit()
-        print()
-        print(f'{choice + 1}| {tag_all[choice].text}')
-        url = newsUrls[choice]
-        getArticleBody(url)
-
-news = News()
-sectionNumDict = {1: news.politics, 2: news.economy, 3: news.society, 4: news.life, 5: news.world, 6: news.science}
+def run(newsType):
+    tag_all = soup.find_all(class_=re.compile("rig.rank"+section[newsType]))
+    newsUrls = []
+    print("\n가장 많이 본 뉴스:"+sectionName[newsType], timeFormat)
+    for i, headline in enumerate(tag_all, start=1):
+        url = base_url + headline.attrs['href']
+        title = headline.text
+        print(f'{i} | {title}\n{url}')
+        newsUrls.append(url)
+    try:
+        choice = int(input('\n읽고 싶으신 기사 번호를 선택하세요: ')) - 1
+    except ValueError:
+        sys.exit()
+    print()
+    print(f'{choice + 1}| {tag_all[choice].text}')
+    url = newsUrls[choice]
+    getArticleBody(url)
 
 try:
-    newsType=int(input("보고싶은 뉴스 번호를 선택하세요:\n1:정치\n2:경제\n3:사회\n4:생활/문화\n5:세계\n6:IT/과학\n👉 "))
+    newsType=int(input("보고싶은 뉴스 번호를 선택하세요:\n1:정치\n2:경제\n3:사회\n4:생활/문화\n5:세계\n6:IT/과학\n👉 ")) - 1
 except ValueError:
     print("1부터 6까지 숫자만 눌러주세요")
     sys.exit()
 
-sectionNumDict[newsType]()
+run(newsType)
 
